e37c5112f6cf2cf253dbc6fab805d72b92e6faa9
Rutendo Mikitayo <rutendomikitayo45@gmail.com>
2025-09-02 15:02:58 +0200

    first commit

--- server/index.ts
+++ server/index.ts
@@ -61,11 +61,13 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
+  const host = process.env.NODE_ENV === "development" ? "localhost" : "0.0.0.0";
+  
   server.listen({
     port,
-    host: "0.0.0.0",
-    reusePort: true,
+    host,
+    reusePort: process.platform !== "win32", // reusePort not supported on Windows
   }, () => {
-    log(`serving on port ${port}`);
+    log(`serving on ${host}:${port}`);
   });
 })();
